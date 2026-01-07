e66d967f09048e91a75796152d82edcea3f44a73
unani676592 <gb676592@gmail.com>
2026-01-07 22:46:48 +0530

    Add resume analyzer components and clean up files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import {resumes} from"../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback tot your fram job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg') bg-cover">
+  <Navbar />
+    <section className= "main-section">
+      <div className="page-heading">
+        <h1>
+          Track Your Applications and & Resume Ratings
+        </h1>
+        <h2>
+          Review your Submission and check AI-powered feedback.
+        </h2>
+      </div>
+    </section>
+    {resumes.length > 0 && (
+        <div className= "resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+    )}
+  </main>
 }
